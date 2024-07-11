3b64df9d31d8009c0089db731357d81c4d8ef18b
marcosbrun0 <Mbrunocampos20@gmail.com>
2024-07-10 21:19:56 -0300

    button more proj

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ document.querySelector('.burger').addEventListener('click', () => {
     document.querySelector('.navbar').classList.toggle('active');
   });
   
+  document.addEventListener("DOMContentLoaded", function() {
+    const btnMostrarMais = document.getElementById("btnMostrarMais");
+    const projetosOcultos = document.querySelectorAll(".projeto.oculto");
+  
+    btnMostrarMais.addEventListener("click", function() {
+      projetosOcultos.forEach(projeto => {
+        projeto.classList.toggle("oculto");
+      });
+  
+      // Alterar o texto do botão após mostrar os projetos
+      if (btnMostrarMais.textContent === "Mostrar Mais Projetos") {
+        btnMostrarMais.textContent = "Mostrar Menos Projetos";
+      } else {
+        btnMostrarMais.textContent = "Mostrar Mais Projetos";
+      }
+    });
+  });
+  
